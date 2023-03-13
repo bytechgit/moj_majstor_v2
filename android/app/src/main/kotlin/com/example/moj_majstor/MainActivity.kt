a1762045a00fe55c49410c9fded24806a134b8c0
Dimitrije Cvetković <dimitrije.le.cvetkovic@gmail.com>
2023-03-13 20:10:02 +0100

    initial_commit

---
+++ android/app/src/main/kotlin/com/example/moj_majstor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.moj_majstor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class UserColor {
+  static const primarybtn = Color.fromRGBO(255, 175, 90, 1);
+  static const background = Color.fromRGBO(251, 251, 251, 1);
+  static const backgorundmuted = Color.fromRGBO(247, 248, 250, 1);
+  static const deepblue = Color.fromRGBO(12, 52, 105, 1);
+}
